f25f83f751d629f782ada795f03f1584045719d8
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-02-27 03:15:36 +0500

    Add files via upload

---
+++ Javascript chp1/app.js
@@ -0,0 +1,11 @@
+//Q1//
+alert("Welcome To My Programming World")
+// Q2 //
+alert("Error! Please enter a valid password.")
+// Q3//
+alert("Welcome to JS Land... \nHappy Coding!")
+// Q4//
+alert("Welcome to JS Land...")
+alert("Happy Coding!")
+// Q5//
+console.log("Hello... I can run JS through my web browser's console")
